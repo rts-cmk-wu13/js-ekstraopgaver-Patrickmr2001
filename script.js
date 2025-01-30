c1dfc670611cac9ba511cf9ed351482f20c731f5
Patrick <patrickmassesson@gmail.com>
2025-01-30 13:37:32 +0100

    til opgave e

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 // Lav dine funktioner her:
 
+// assignmentA: Tjekker om to værdier er ens i både værdi og type
+function assignmentA(a, b) {
+    return a === b;
+}
+
+// assignmentB: Returnerer de tre sidste tegn i en given string
+function assignmentB(string) {
+    return string.slice(-3);
+}
+
+// assignmentC: Tjekker om et tal er lige
+function assignmentC(number) {
+    return number % 2 === 0;
+}
+
+// assignmentD: Finder det mindste tal i et array, som er større end 0
+function assignmentD(array) {
+    return Math.min(...array.filter(num => num > 0));
+}
+
+// assignmentE: Kan skælne imellem ord med b og B
+function assignmentE(array) {
+    return string ("bil", "banan", "Bord", "hest", "abe", "Kaffe");
+    string = string.toLowerCase();
+
+}
